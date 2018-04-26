ff98a8fc07e65b687d0150f9526e6e053df0867f
Grahame Bowland <grahame@angrygoats.net>
2018-04-26 21:00:57 +0800

    update for latest ealgis-common

--- recipe.py
+++ recipe.py
@@ -10,7 +10,7 @@ logger = make_logger(__name__)
 def main():
     tmpdir = "/app/tmp"
     census_dir = '/app/data/2011 Datapacks BCP_IP_TSP_PEP_ECP_WPP_ERP_Release 3'
-    factory = DataLoaderFactory("scratch_census_2011", clean=False)
+    factory = DataLoaderFactory(db_name="scratch_census_2011", clean=False)
     shape_result = load_shapes(factory, census_dir, tmpdir)
     attrs_results = load_attrs(factory, census_dir, tmpdir)
     for result in [shape_result] + attrs_results:
